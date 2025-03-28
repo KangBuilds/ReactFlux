60c67797912e227d06b5d7e94097b3e85f9fb3ee
Vi <weikang0827@gmail.com>
2025-03-28 00:57:52 +0000

    chore: remove version check

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Button, Layout, Notification } from "@arco-design/web-react"
+import { Layout } from "@arco-design/web-react"
 import { useStore } from "@nanostores/react"
 import { useEffect } from "react"
 
@@ -9,16 +9,12 @@ import useFeedIconsSync from "./hooks/useFeedIconsSync"
 import useLanguage, { polyglotState } from "./hooks/useLanguage"
 import useScreenWidth from "./hooks/useScreenWidth"
 import useTheme from "./hooks/useTheme"
-import useVersionCheck from "./hooks/useVersionCheck"
-import { GITHUB_REPO_PATH } from "./utils/constants"
 import hideSpinner from "./utils/loading"
 
 const App = () => {
   useLanguage()
   useTheme()
   useFeedIconsSync()
-
-  const { hasUpdate, dismissUpdate } = useVersionCheck()
 
   const { isBelowLarge } = useScreenWidth()
 
@@ -27,44 +23,6 @@ const App = () => {
   useEffect(() => {
     hideSpinner()
   }, [])
-
-  useEffect(() => {
-    if (hasUpdate) {
-      const id = "new-version-available"
-      Notification.info({
-        id,
-        title: polyglot.t("app.new_version_available"),
-        closable: false,
-        content: polyglot.t("app.new_version_available_description"),
-        duration: 0,
-        btn: (
-          <span>
-            <Button
-              size="small"
-              style={{ marginRight: 8 }}
-              type="secondary"
-              onClick={() => {
-                dismissUpdate()
-                Notification.remove(id)
-              }}
-            >
-              {polyglot.t("actions.dismiss")}
-            </Button>
-            <Button
-              size="small"
-              type="primary"
-              onClick={() => {
-                window.open(`https://github.com/${GITHUB_REPO_PATH}/commits/main`, "_blank")
-                Notification.remove(id)
-              }}
-            >
-              {polyglot.t("actions.check")}
-            </Button>
-          </span>
-        ),
-      })
-    }
-  }, [dismissUpdate, hasUpdate, polyglot])
 
   return (
     polyglot && (
